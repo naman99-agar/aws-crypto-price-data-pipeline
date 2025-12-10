db879ef1dfa2db1fd9138be9cca3dd89d5f17b50
naman99-agar <68807083+naman99-agar@users.noreply.github.com>
2025-12-10 21:38:12 +0530

    Update dashboard.py

---
+++ backend/dashboard.py
@@ -0,0 +1,21 @@
+import boto3
+import pandas as pd
+import plotly.express as px
+
+BUCKET = "your bucket name"
+KEY = "processed/prices.csv"
+
+s3 = boto3.client("s3")
+
+# Download CSV from S3
+s3.download_file(BUCKET, KEY, "prices.csv")
+
+# Read CSV
+df = pd.read_csv("prices.csv")
+
+# Convert timestamp to datetime
+df["timestamp"] = pd.to_datetime(df["timestamp"], format="%Y-%m-%d_%H-%M-%S")
+
+# Plot
+fig = px.line(df, x="timestamp", y="price_usd", title="Bitcoin Price Over Time")
+fig.show()
