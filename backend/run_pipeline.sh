05803e798d3470f70126ea5beb2d239bdd06698c
naman99-agar <68807083+naman99-agar@users.noreply.github.com>
2025-12-10 21:39:20 +0530

    Update run_pipeline.sh

---
+++ backend/run_pipeline.sh
@@ -0,0 +1,6 @@
+# Activates virtual environment and runs main.py
+source /Users/namanagarwalla/Documents/Projects/aws-data-pipeline/backend/venv/bin/activate
+python /Users/namanagarwalla/Documents/Projects/aws-data-pipeline/backend/main.py
+
+# Make it executable
+chmod +x run_pipeline.sh
